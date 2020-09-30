7f0bf529594a31bd83888f17622b62d80bb586de
DavidRMorphew <morphewd@umich.edu>
2020-09-29 20:23:19 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song(title)
+        song = Song.new(title)
+    end
+end
