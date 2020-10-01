f4c69b2cc84c0803be7e7593abbb45959fceea59
DavidRMorphew <morphewd@umich.edu>
2020-10-01 00:01:19 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,20 @@ class Song
         @@all
     end
 
-    def self.new_by_filename(filename)
-        
+    def artist_name=(artist_name)
+        # self.artist = Artist.find_or_create_by_name(artist_name)
+        Artist.find_or_create_by_name(artist_name).add_song(self)
     end
 
-    def artist_name=(artist_name)
-        self.artist = Artist.find_or_create_by_name(artist_name)
+    def self.new_by_filename(filename)
+        # split_filename = filename.split(" - ")
+        # new_song = self.new(split_filename[1]) if self.all.none? {|song| song.name == split_filename[1]}
+        # song_artist = split_filename[0]
+        # # binding.pry
+        # new_song.artist_name = song_artist
+        # new_song
+        new_song = self.new(filename.split(" - ")[1])
+        new_song.artist_name = filename.split(" - ")[0]
+        new_song
     end
 end
