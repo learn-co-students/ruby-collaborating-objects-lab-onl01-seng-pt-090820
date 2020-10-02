4703eace6c596a81a3df15f2031969dd388ba44e
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-02 22:33:28 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,19 @@
 class MP3Importer 
+
+attr_accessor :path
   
-  def initialize(file) 
+  def initialize(file_path) 
+    @path = file_path
   end
   
   def files
-    
+    filenames = Dir.chdir(@path){|path| Dir.glob("*.mp3")}
+    filenames
   end
   
-  def import(file_name)
-    
+  def import
+    filenames = self.files
+    filenames.each{|filename| Song.new_by_filename(filename)}
   end
     
-  
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Song 
   
-  attr_accessor :name, :artist 
+  attr_accessor :name, :artist
   
   @@all = []
   
@@ -16,7 +16,8 @@ class Song
   
   def self.new_by_filename(file)
     song = Song.new(file.split(" - ")[1])
-    artist.name = Artist.find_or_create_by_name(file.split(" - ")[0])
+    song.artist_name=(file.split(" - ")[0])
+    song
   end 
   
   def artist_name=(name)
